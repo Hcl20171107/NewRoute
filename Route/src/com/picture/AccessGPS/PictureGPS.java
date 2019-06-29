d0b9987a66f9f89ebb2cf3a8dcb296e8037caa59
Hcl20171107 <43674629+Hcl20171107@users.noreply.github.com>
2019-06-30 01:05:34 +0800

    简单的画线，并不完善

--- Route/src/com/picture/AccessGPS/PictureGPS.java
+++ Route/src/com/picture/AccessGPS/PictureGPS.java
@@ -1,5 +1,109 @@
 package com.picture.AccessGPS;
 
-public class PictureGPS {
+import java.io.File;
+import java.io.IOException;
 
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
+
+
+
+public class PictureGPS {
+	public static void main(String[] args) throws Exception, Exception{  
+        File file = new File("C:\\Users\\hp\\Desktop\\张阳阳.jpg"); 
+        File file1 = new File("C:\\Users\\hp\\Desktop\\1.jpg");  
+        printImageTags(file);
+        printImageTags(file1);
+    } 
+	
+	private static void printImageTags(File file) throws ImageProcessingException, Exception{  
+    	Metadata metadata = ImageMetadataReader.readMetadata(file);  
+        for (Directory directory : metadata.getDirectories()) {  
+            for (Tag tag : directory.getTags()) {  
+                String tagName = tag.getTagName();  
+                String desc = tag.getDescription(); 
+                if (tagName.equals("Image Height")) {  
+                	System.out.println("图片高度: "+desc);
+                } else if (tagName.equals("Image Width")) {  
+                	System.out.println("图片宽度: "+desc);
+                } else if (tagName.equals("Date/Time Original")) {  
+                	System.out.println("拍摄时间: "+desc);
+                }else if (tagName.equals("GPS Latitude")) {  
+                	System.err.println("纬度 : "+desc);
+                } else if (tagName.equals("GPS Longitude")) {  
+                	System.err.println("经度: "+desc);
+                }
+            }  
+        }  
+    }  
+   
+    public static String pointToLatlong (String point ) {  
+        Double du = Double.parseDouble(point.substring(0, point.indexOf("°")).trim());  
+        Double fen = Double.parseDouble(point.substring(point.indexOf("°")+1, point.indexOf("'")).trim());  
+        Double miao = Double.parseDouble(point.substring(point.indexOf("'")+1, point.indexOf("\"")).trim());  
+        Double duStr = du + fen / 60 + miao / 60 / 60 ;  
+        return duStr.toString();  
+    }  
+
+	/*public ImgInfoBean parseImgInfo(String fileName)
+	{
+	   File file = new File(fileName);
+	    ImgInfoBean imgInfoBean = null;    
+	    try {
+	        Metadata metadata = ImageMetadataReader.readMetadata(file);
+	        imgInfoBean = printImageTags(file, metadata);
+	    } catch (ImageProcessingException e) {
+	        System.err.println("error 1a: " + e);
+	    } catch (IOException e) {
+	        System.err.println("error 1b: " + e);
+	    }
+	    return imgInfoBean;
+	}
+
+
+		public ImgInfoBean printImageTags(File sourceFile, Metadata metadata)
+		{
+		    ImgInfoBean imgInfoBean = new ImgInfoBean ();
+		    imgInfoBean.setImgSize(sourceFile.getTotalSpace());
+		    imgInfoBean.setImgName(sourceFile.getName());
+		    for (Directory directory : metadata.getDirectories()) {
+		        for (Tag tag : directory.getTags()) {
+		            String tagName = tag.getTagName();
+		            String desc = tag.getDescription();
+		            if (tagName.equals("Image Height")) {
+		                //图片高度
+		                imgInfoBean.setImgHeight(desc);
+		            } else if (tagName.equals("Image Width")) {
+		                //图片宽度
+		                imgInfoBean.setImgWidth(desc);
+		            } else if (tagName.equals("Date/Time Original")) {
+		                //拍摄时间
+		                imgInfoBean.setDateTime(desc);
+		            } else if (tagName.equals("GPS Altitude")) {
+		                //海拔
+		                imgInfoBean.setAltitude(desc);
+		            } else if (tagName.equals("GPS Latitude")) {
+		                //纬度
+		                imgInfoBean.setLatitude(pointToLatlong(desc));
+		            } else if (tagName.equals("GPS Longitude")) {
+		                //经度
+		                imgInfoBean.setLongitude(pointToLatlong(desc));
+		            }
+		        }
+		        for (String error : directory.getErrors()){
+		            System.err.println("ERROR: " + error);
+		        }
+		    }
+		    return imgInfoBean;
+		}
+		public String pointToLatlong (String point ) {
+		    Double du = Double.parseDouble(point.substring(0, point.indexOf("°")).trim());
+		    Double fen = Double.parseDouble(point.substring(point.indexOf("°")+1, point.indexOf("'")).trim());
+		    Double miao = Double.parseDouble(point.substring(point.indexOf("'")+1, point.indexOf("\"")).trim());
+		    Double duStr = du + fen / 60 + miao / 60 / 60 ;
+		    return duStr.toString();
+		}*/
 }

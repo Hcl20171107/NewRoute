ae9f4234e06f45443253fdc082b9d3160fba3551
Hcl20171107 <43674629+Hcl20171107@users.noreply.github.com>
2019-06-29 22:30:51 +0800

    Update MapDraw.java

--- Route/src/com/picture/Draw/MapDraw.java
+++ Route/src/com/picture/Draw/MapDraw.java
@@ -1,5 +1,16 @@
 package com.picture.Draw;
 
+import java.io.File;
+
 public class MapDraw {
+	public static void main(String[] args) throws Exception, Exception{ 
+		File file = new File("C:\\Users\\hp\\Desktop\\’≈—Ù—Ù.jpg");
+		printImageTags(file); 
+		}
+
+	private static void printImageTags(File file) {
+		// TODO Auto-generated method stub
+		
+	} 
 
 }
